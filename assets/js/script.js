152d4150c77ce52492db0ccab35ced1b3fb12f1e
Jason <jasonpsmith180@gmail.com>
2021-02-08 22:44:38 -0700

    add form reset after task submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    // Reset form input fields
+    formEl.reset();
+
     // Package data as an object
     var taskDataObj = {
         name: taskNameInput,
